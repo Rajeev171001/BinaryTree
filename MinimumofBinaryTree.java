e8d25170d20aa5c80a49773aa34ef2e90334a2ae
Rajeev Sharma <112720734+Rajeev171001@users.noreply.github.com>
2022-11-03 19:34:32 +0530

    Minimum value of node

---
+++ MinimumofBinaryTree.java
@@ -0,0 +1,9 @@
+static int maximum(Node root){
+		if(root==null) return Integer.MIN_VALUE;
+
+		return Math.max(root.data,Math.max(maximum(root.left),maximum(root.right)));
+	}
+
+//minimum value of node
+//call this function in main function
+//first make binary tree
